aa85ffdc47abd93292ef4e1063eb2efb21c5344b
Assad-lz <assadyssaky@gmail.com>
2025-02-06 13:20:14 -0300

    feat: LP is ready to deploy

--- src/script.js
+++ src/script.js
@@ -3,5 +3,5 @@ const mobileMenu = document.getElementById("mobile-menu");
 const navUl = document.querySelector("nav ul");
 
 mobileMenu.addEventListener("click", () => {
-  navUl.classList.toggle("active");
+  navUl.classList.toggle("active"); // Adiciona/remove a classe "active"
 });
